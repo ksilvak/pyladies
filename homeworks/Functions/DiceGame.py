c8ce65ff2165c7f066d22645b12c5fac5f105170
Silva <silvie.kucerova@mergado.com>
2020-07-12 23:05:07 +0200

    review corrections

--- homeworks/Functions/DiceGame.py
+++ homeworks/Functions/DiceGame.py
@@ -9,10 +9,10 @@ player1 = 0
 player2 = 0
 player3 = 0
 player4 = 0
-player_one_rounds = 1
-player_two_rounds = 1
-player_three_rounds = 1
-player_four_rounds = 1
+player_one_rounds = 0
+player_two_rounds = 0
+player_three_rounds = 0
+player_four_rounds = 0
 
 while player1 != 6:
     player1 = dice_roll()
@@ -34,7 +34,7 @@ while player4 != 6:
     player_four_rounds = player_four_rounds + 1
     print("Hráč 4: " + str(player4))
 
-print('Počet hodů: Hráč 1: ', player_one_rounds - 1, ', Hráč 2: ', player_two_rounds, ', Hráč 3: ', player_three_rounds, ', Hráč 4: ', player_four_rounds)
+print('Počet hodů: Hráč 1: ', player_one_rounds, ', Hráč 2: ', player_two_rounds, ', Hráč 3: ', player_three_rounds, ', Hráč 4: ', player_four_rounds)
 
 if player_one_rounds > player_two_rounds and player_one_rounds > player_three_rounds and player_one_rounds > player_four_rounds:
     print('Vyhrál Hráč 1')
--- homeworks/Functions/Refactoring.py
+++ homeworks/Functions/Refactoring.py
@@ -4,7 +4,7 @@ import numbers
 personal_id = input('Zadejte Vaše rodné číslo: ')
 
 def is_format_ok(personal_id):
-    if personal_id[0:6].isdigit() & personal_id[7:11].isdigit():
+    if personal_id[0:6].isdigit() and personal_id[7:11].isdigit():
         if personal_id[6] == '/':
             return True
         else:
@@ -16,19 +16,15 @@ print(is_format_ok(personal_id))
 
 # Is divisible by eleven
 
-personal_id = input('Zadejte Vaše rodné číslo: ')
-
 def is_divisible(personal_id, n):
     number = int(personal_id[0:6] + personal_id[7:11])
     if number % n == 0:
         return True
     else:
-        return False 
+        return False
 print(is_divisible(personal_id, 11))
 
 # Gender
-personal_id = input('Zadejte Vaše rodné číslo: ')
-
 def gender(personal_id):
     if int(personal_id[2:4]) - 50 > 0:
         return 'Žena'
@@ -37,13 +33,11 @@ def gender(personal_id):
 print(gender(personal_id))
 
 # Birthday
-personal_id = input('Zadejte Vaše rodné číslo: ')
-
 def birth_day(personal_id):
     day = personal_id[4:6]
     month = personal_id[2:4]
     year = personal_id[0:2]
-    
+
     if int(personal_id[2:4]) - 50 > 0:
         month = int(personal_id[2:4]) - 50
         birth = [day, month, year]
